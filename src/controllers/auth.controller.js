cb5d3bfd6b3ae918c146732894639b2d991fb3a4
Atharva876 <atharvaberad07@gmail.com>
2026-02-11 17:24:37 +0530

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,1 +1,13 @@
 const userModel = require("../models/user.model")
+
+/* 
+* User register Controller
+* POST /api/auth/register
+
+*/
+
+function userRegisterController(req,res){
+
+    const{email,name, password} = req.body
+    ss
+}
