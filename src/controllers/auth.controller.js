3df36d3ec3c21785d2fd727c84cfccc5c8db273f
Atharva <atharvaberad8@gmail.com>
2026-02-13 17:32:15 +0530

    nodemailer configured

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const  emailService = require("../services/email.service")
 /* 
 * User register Controller
 * POST /api/auth/register
@@ -36,6 +37,7 @@ async function userRegisterController(req,res)
         },
         token
     })
+    await emailService.sendRegistrationEmail(user.email, user.name);
 
 }
 
